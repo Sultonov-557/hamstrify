c101ab8c3c271a09ab1e734554255ee8e56a41ce
Sultonov-557 <sultonovzerifboy@gmail.com>
2024-07-11 14:13:36 +0500

    .

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import tokens from "./tokens.json" assert { type: "json" };
+import { readFileSync } from "fs";
 import { Hamster } from "./hamster.js";
 
+const tokens = JSON.parse(readFileSync("./tokens.json").toString());
+
 for (let token of tokens) {
-	const hamster = new Hamster(token);
-	setInterval(() => hamster.update(), 1000 * 60 * 3);
+  const hamster = new Hamster(token);
+  setInterval(() => hamster.update(), 1000 * 60 * 3);
 }
